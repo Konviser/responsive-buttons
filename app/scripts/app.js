a36eae8097a877df264a042eeaa59f100641404b
Konviser <svetsapelova@gmail.com>
2014-11-08 11:39:00 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name responsiveButtonsApp
+ * @description
+ * # responsiveButtonsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('responsiveButtonsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name responsiveButtonsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the responsiveButtonsApp
+ */
+angular.module('responsiveButtonsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
